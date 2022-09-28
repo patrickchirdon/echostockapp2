238faafb041c73f9d9c4040654203717f8af4e15
Patrick Chirdon <39843493+patrickchirdon@users.noreply.github.com>
2022-09-28 18:48:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function call_api(finishedAPI, ticker){
     }
     
 	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_2711a2706e924888a2a063e6e4cf4307', {json: true}, (err, res,body)=> {
-	if(err){ return console.log(err);}
+	if(err){ return console.log('err');}
 	if(res.statusCode ===200){
 	   
     const myLogger = new Console({
@@ -54,7 +54,7 @@ function call_api(finishedAPI, ticker){
         
         
 		finishedAPI(body);
-		return console.log(err);
+		return console.log('err');
 		}
 	});
 	
@@ -66,7 +66,7 @@ function call_api(finishedAPI, ticker){
 	
 	//advanced stats
 	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/stats?token=pk_2711a2706e924888a2a063e6e4cf4307', {json: true}, (err, res,body)=> {
-	if(err){ return console.log(err);}
+	if(err){ return console.log('err');}
 	if(res.statusCode ===200){
 	     
     const myLogger = new Console({
@@ -75,7 +75,7 @@ function call_api(finishedAPI, ticker){
     });
 	    myLogger.log(JSON.stringify(body))
 		
-	    return console.log(err);
+	    return console.log('err');
 	    
 		}
 	});
@@ -87,7 +87,7 @@ function call_api(finishedAPI, ticker){
     
 	//company info
 	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/company?token=pk_2711a2706e924888a2a063e6e4cf4307', {json: true}, (err, res,body)=> {
-	if(err){ return console.log(err);}
+	if(err){ return console.log('err');}
 	if(res.statusCode ===200){
 	     
     const myLogger = new Console({
@@ -98,7 +98,7 @@ function call_api(finishedAPI, ticker){
 	    
 		}
 		
-		return console.log(err);
+		return console.log('err');
 	});
 	
 	if (tickervar===0){
@@ -108,7 +108,7 @@ function call_api(finishedAPI, ticker){
     
 	//company news
 	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/news/last/4?token=pk_2711a2706e924888a2a063e6e4cf4307', {json: true}, (err, res,body)=> {
-	if(err){ return console.log(err);}
+	if(err){ return console.log('err');}
 	if(res.statusCode ===200){
 	     
         const myLogger = new Console({
@@ -120,7 +120,7 @@ function call_api(finishedAPI, ticker){
 	    
 		}
 		
-		return console.log(err);
+		return console.log('err');
 	});
 	
   
@@ -214,7 +214,7 @@ app.post('/example', (req, res) => {
 
     
     
- return console.log(err);
+ return console.log('err');
   //...
   res.end()
 })
@@ -237,7 +237,7 @@ app.get('/', function(req, res){
 		
 		
 	});
-	return console.log(err);
+	return console.log('err');
 
 });
 
@@ -257,7 +257,7 @@ app.post('/', function(req, res){
 		
 		});
 	}, req.body.stock_ticker);
-	return console.log(err);
+	return console.log('err');
 
 });
 
